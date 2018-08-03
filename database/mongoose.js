72eb78f550b34eb2e635c809f8093401f3bd40a9
barman47 <nomsouzoanya@yahoo.co.uk>
2018-08-03 10:40:47 +0100

    mLab config

--- database/mongoose.js
+++ database/mongoose.js
@@ -1,14 +1,9 @@
 const mongoose = require('mongoose');
 
 mongoose.Promise = global.Promise;
-mongoose.connect('mongodb://ds111492.mlab.com:11492/logbookapp',
-                 {user: 'barman47', pass: 'VICEcity@47'},
-                 () => {
-                     console.log('Database Connected');
-                 });
-// mongoose.connect('mongodb://barman47:VICEcity@47@ds111492.mlab.com:11492/logbookapp', {
-//     useNewUrlParser: true
-// });
+mongoose.connect('mongodb://barman47:VICEcity%40@ds111492.mlab.com:11492/logbookapp', {
+    useNewUrlParser: true
+});
 
 console.log('Connected to Mongo Database on mongodb://localhost:27017/LogbookApp');
 
