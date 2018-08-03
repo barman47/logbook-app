1e4ab51f40821246349c45e6da6d1f6477762c55
barman47 <nomsouzoanya@yahoo.co.uk>
2018-08-03 15:54:45 +0100

    mLab config

--- database/mongoose.js
+++ database/mongoose.js
@@ -5,10 +5,12 @@ mongoose.Promise = global.Promise;
 //     useNewUrlParser: true
 // });
 
-mongoose.connect('mongodb://barman47:VICEcity@47@ds111492.mlab.com:11492/logbookapp', {
+mongoose.connect('mongodb://barman47:VICEcity47@ds111492.mlab.com:11492/logbookapp', {
     useNewUrlParser: true
 });
 console.log('Connected to Mongo Database on mongodb://localhost:27017/LogbookApp');
+
+
 
 module.exports = {mongoose};
 
