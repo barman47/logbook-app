b83207dd380ce8fe270b81961add65ddfa890635
barman47 <nomsouzoanya@yahoo.co.uk>
2018-08-03 09:40:58 +0100

    mongoose config for mLab

--- database/mongoose.js
+++ database/mongoose.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose');
 
 mongoose.Promise = global.Promise;
-mongoose.connect('mongodb://localhost:27017/LogbookApp', {
+mongoose.connect('mongodb://barman47:VICEcity@47@ds111562.mlab.com:11562/heroku_2110v6rs', {
     useNewUrlParser: true
 });
 
