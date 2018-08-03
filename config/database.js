c1a286e0bc14c8349a72694c46edcc9e70c05a34
barman47 <nomsouzoanya@yahoo.co.uk>
2018-08-03 09:29:59 +0100

    Added mLab config

--- config/database.js
+++ config/database.js
@@ -1,4 +1,5 @@
 module.exports = {
-    database: 'mongodb://localhost:27017/LogbookApp',
+    // database: 'mongodb://localhost:27017/LogbookApp',
+    database: 'mongodb://barman47:VICEcity@47@ds111562.mlab.com:11562/heroku_2110v6rs',
     secret: 'barman secret'
 };
