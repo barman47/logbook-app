214ca10a580505e1da102ecc0385dca5d0d27f0b
barman47 <nomsouzoanya@yahoo.co.uk>
2018-08-03 10:55:08 +0100

    mLab config

--- database/mongoose.js
+++ database/mongoose.js
@@ -1,7 +1,8 @@
 const mongoose = require('mongoose');
+const uri = 'mongodb://barman47:vicecity47@ds111492.mlab.com:11492/logbookapp';
 
 mongoose.Promise = global.Promise;
-mongoose.connect('mongodb://barman47:vicecity47@ds111492.mlab.com:11492/logbookapp', {
+mongoose.connect(uri, {
     useNewUrlParser: true
 });
 console.log('Connected to Mongo Database on mongodb://localhost:27017/LogbookApp');
